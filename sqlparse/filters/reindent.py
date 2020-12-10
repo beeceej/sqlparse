bdf425f70fc725d42bee9666090330e1bb83a1de
beeceej <beeceej@hey.com>
2020-12-09 22:15:50 -0500

    Jinja: Format Jinja blocks newlines
    If the jinja block contains newlines, then we attempt to format the block on it's own

--- sqlparse/filters/reindent.py
+++ sqlparse/filters/reindent.py
@@ -109,6 +109,24 @@ class ReindentFilter:
         with indent(self):
             self._process_default(tlist)
 
+    def _process_jinja(self, tlist):
+        ttypes = T.Keyword.DML, T.Keyword.DDL
+        _, is_dml_dll = tlist.token_next_by(t=ttypes)
+        fidx, first = tlist.token_next_by(m=sql.Jinja.M_OPEN)
+        print("tlist", tlist)
+        if first is None:
+            return
+        _, has_new_lines = tlist.token_next_by(t=T.Newline)
+
+        if has_new_lines:
+            tlist.tokens.insert(1, self.nl())
+            tlist.tokens.insert(-1, self.nl())
+
+        with indent(self, 1 if is_dml_dll else 0):
+            tlist.tokens.insert(0, self.nl()) if is_dml_dll else None
+            with offset(self, self._get_offset(first) + 1):
+                self._process_default(tlist, not is_dml_dll)
+
     def _process_parenthesis(self, tlist):
         ttypes = T.Keyword.DML, T.Keyword.DDL
         _, is_dml_dll = tlist.token_next_by(t=ttypes)
